07aff789f7b3aa24cd57c73240ff11ce3347a3ab
M1rad0 <139159250+M1rad0@users.noreply.github.com>
2024-02-12 23:27:35 +0100

    Modele accueil Utilisateur

--- inc/php/connex.php
+++ inc/php/connex.php
@@ -3,7 +3,7 @@ function bdconnect()
 {
     static $connect = null;
     if ($connect === null) {
-        $connect = mysqli_connect('192.168.47.45' , 'kiady' , '' , 'the');   
+        $connect = mysqli_connect('172.20.6.164' , 'kiady' , '' , 'the');   
     }
     return $connect;
 }
