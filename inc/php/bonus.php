2fe72ee9b706898914f4175be8096f4262a78040
HenintsoaRazafinjatovo <152251886+HenintsoaRazafinjatovo@users.noreply.github.com>
2024-02-13 15:03:20 +0000

    gestion bonus & traitement bonus

--- inc/php/bonus.php
+++ inc/php/bonus.php
@@ -1,4 +1,5 @@
 <?php
+
 function surplus($poidsMin,$poidsAzo,$salKilo,$pourcentageBonus){
     $azahonaBonus=$poidsAzo-$poidsMin;
     return $azahonaBonus*$pourcentageBonus/100;
@@ -7,4 +8,25 @@ function sousMoins($poidsMin,$poidsAzo,$salKilo,$pourcentageMallus){
     $azahonaMallus=$poidsAzo-$poidsMin;
     return $azahonaMallus*$pourcentageMallus/100;
 } 
+function bonus($cueillette){
+    $config=selectAll("The_ConfigPoids WHERE id in (SELECT MAX(id) FROM The_ConfigPoids)");
+    $salkilo=selectAll("The_SalKilo WHERE id in (SELECT MAX(id) FROM The_SalKilo)");
+
+    $cueillette['commission']=$cueillette['poids']*$salKilo;
+    if($config['poidsMinimal']==$cueillette['poids']){
+        $cueillette['bonus']=0;
+        $cueillette['mallus']=0;
+    }
+    else if($poidsMin>$cueillette['poids']){
+        $cueillette['mallus']=sousMoins($config['poidsMinimal'],$cueillette['poids'],$salkilo,$config['mallus']);
+        $cueillette['bonus']=0;
+
+    }
+    else if($poidsMin<$cueillette['poids']){
+        $cueillette['bonus']=surplus($config['poidsMinimal'],$cueillette['poids'],$salkilo,$config['bonus']);
+        $cueillette['mallus']=0;
+
+    }
+
+}
 
