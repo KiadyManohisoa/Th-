47e521c7ee9e6c69d6919fa296525cd242f21eb6
Kiady <kiadyandriamanantsilavo@gmail.com>
2024-02-14 16:44:29 +0300

    Update fonction.php

--- inc/php/function.php
+++ inc/php/function.php
@@ -1,9 +1,22 @@
 <?php
+    function isValueOfKeyInTab($key, $object, $table) { 
+        foreach($table as $cle=>$value){
+            if($value[$key]==$object){
+                return true;
+            }
+        }
+        return false;
+    }
+
     include 'connex.php';
     include 'admin.php'; 
     include 'utilisateur.php';
     include 'crud.php';
     include 'cueillette.php';
     include 'resultat.php';
+<<<<<<< HEAD
     include 'payement.php';
+=======
+    include 'bonus.php';
+>>>>>>> 09b6ea12e9884d9ebcf78b6eb5e69b27fd76f801
 ?>
